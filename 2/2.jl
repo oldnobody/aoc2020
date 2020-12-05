3ae3da1ab74044c83f2a34a580618bacf2e92611
oldnobody <bassbild@fastmail.fm>
2020-12-05 14:55:02 +0900

    solved day2-1

--- 2/2.jl
+++ 2/2.jl
@@ -1,2 +1,29 @@
 input=readlines("input.txt")
-println(input)
+
+function numberOfOccurences(char, word)
+  count = 0
+  for c in word
+    if c==char
+      count += 1
+    end
+  end
+  return count
+end
+
+function day2()
+  counter = 0
+
+  for line in input
+    min, max, char, word = split(line, ['-', ' ']) 
+
+
+    n = numberOfOccurences(char[1], word)
+    if n >= parse(Int, min) && n <= parse(Int, max)
+      counter += 1
+    end
+  end
+
+  println(counter)
+end
+
+day2()
