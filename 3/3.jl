348a39f39b569de0ad556d85d4d9ad3d37788303
oldnobody <bassbild@fastmail.fm>
2020-12-05 16:31:28 +0900

    solved day3-1

--- 3/3.jl
+++ 3/3.jl
@@ -3,10 +3,24 @@ input = readlines("input.txt")
 function day3()
   count = 0
 
-  for line in input
-  
+  for (i, line) in enumerate(input)
 
+      n = mod(i,31) 
+      x = mod(i*3,31) + 1 
+      y = i + 1
+
+      if (y<=length(input))
+        c = input[y][x]
+        #println("i: $i, x: $x, y: $y, c: $c")
+
+        if c == '#'
+          count += 1
+        end
+      end
   end
+
+  
+  println(count)
 end
 
 day3()
