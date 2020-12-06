7d500f891d8a1552ded89f49e0169efd3f011d56
oldnobody <bassbild@fastmail.fm>
2020-12-06 10:28:03 +0900

    solved day3

--- 3/3.jl
+++ 3/3.jl
@@ -1,11 +1,10 @@
 input = readlines("input.txt")
 
-function day3()
+function day3_1()
   count = 0
 
   for (i, line) in enumerate(input)
 
-      n = mod(i,31) 
       x = mod(i*3,31) + 1 
       y = i + 1
 
@@ -23,4 +22,46 @@ function day3()
   println(count)
 end
 
-day3()
+function calculateSlope(data, slope)
+  
+  height = length(data)
+  width = length(data[1])
+  start = (1,1)
+  trees = 0
+
+  for i=0:height-1
+    
+    x = start[1] + (slope[1] * i) % width
+    y = start[2] + (slope[2] * i)
+
+    if y > height
+      #println("y is $y, continue")
+      continue
+    end
+
+    if data[y][x] == '#'
+      trees += 1
+    end
+
+  end
+
+  return trees
+
+end
+
+function day3_2()
+  slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+  count = 0
+  product = 1
+  
+  for slope in slopes
+    count = calculateSlope(input, slope)
+    product *= count
+    count = 0
+  end
+
+  println(product)
+end
+
+#day3_1()
+day3_2()
